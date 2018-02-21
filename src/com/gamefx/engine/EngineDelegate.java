48c43dbbd7cdef001cc4a2595145473f014b8a47
cascau <cascau@rocketmail.com>
2018-02-21 23:48:28 +0200

    Improved movement animation

--- src/com/gamefx/engine/EngineDelegate.java
+++ src/com/gamefx/engine/EngineDelegate.java
@@ -11,14 +11,18 @@ public class EngineDelegate {
 
     public void moveGameObjetToPoint(GameObject entity, Point2D destination) {
 
+        double speed = 0.2;
+        double distance = destination.distance(new Point2D(entity.getTranslateX(), entity.getTranslateY()));
+        double time = distance / speed;
+
         Timeline timeline = new Timeline(60);
         // timeline that scales and moves the player
         timeline.getKeyFrames().clear();
         timeline.getKeyFrames().addAll(
-                new KeyFrame(Duration.millis(1000), new KeyValue(entity.translateXProperty(), destination.getX())),
-                new KeyFrame(Duration.millis(1000), new KeyValue(entity.translateYProperty(), destination.getY())),
-                new KeyFrame(Duration.millis(1000), new KeyValue(entity.scaleXProperty(), entity.getScaleX())),
-                new KeyFrame(Duration.millis(1000), new KeyValue(entity.scaleYProperty(), entity.getScaleY()))
+                new KeyFrame(Duration.millis(time), new KeyValue(entity.translateXProperty(), destination.getX())),
+                new KeyFrame(Duration.millis(time), new KeyValue(entity.translateYProperty(), destination.getY())),
+                new KeyFrame(Duration.millis(time), new KeyValue(entity.scaleXProperty(), entity.getScaleX())),
+                new KeyFrame(Duration.millis(time), new KeyValue(entity.scaleYProperty(), entity.getScaleY()))
         );
         timeline.play();
     }
